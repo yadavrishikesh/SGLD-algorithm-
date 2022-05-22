cab891dfeddadb979bf716e37ff77fae18bab928
Rishikesh Yadav <49691784+yadavrishikesh@users.noreply.github.com>
2022-05-22 17:28:13 +0300

    Update Data_and_initial_values.R

--- Data_and_initial_values.R
+++ Data_and_initial_values.R
@@ -9,7 +9,7 @@ library(fields)
 #Product mixture model: Y(s)= alpha(s) X1(s) * X2 * X3(s), where: gamma(x) is the gamma function
 # X1(s): i.i.d Weibull(1/beta1,1/gamma(1+beta1)), with shape 1/beta1 and scale 1/gamma(1+beta1)
 # X2 spatially constant values (varying with respect to time only) with marginal Weibull(1/beta2,1/gamma(1+beta2))
-# X3(s): marginally Inv-Gamma(beta3,beta3-1), with rate beta3-1 >0 and shape beta3 > 1, dependence structure governed by Gaussian copula with exponential correlation function exp(-h/rho), h>=0, with range rho>0
+# X3(s): marginally Inv-Gamma(beta3,beta3-1), with scale beta3-1 >0 and shape beta3 > 1, dependence structure governed by Gaussian copula with exponential correlation function exp(-h/rho), h>=0, with range rho>0
 # alpha(s): scale parameter of the model with exp(gamma0 + gamma1 * Z1(s) + gamma2 * Z2(s) + gamma3 * Z3(s)), where Zi(s), i=1,2,3 are the spatial covariates 
 
 ###################################
